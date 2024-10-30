26567cb7e714e2728b4cb74a46067f20e1e4a3b0
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-30 17:56:25 -0400

    Agregar lista en la clase AlgoUiState

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/algo/AlgoUiState.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/algo/AlgoUiState.kt
@@ -1,11 +1,13 @@
 package edu.ucne.proyectofinalaplicada2.presentation.algo
 
+import edu.ucne.proyectofinalaplicada2.data.local.entities.AlgoEntity
+
 data class AlgoUiState(
     val algoId: Int? = null,
-    val nombre: String = "",
-    val tipo: String = "",
-    //val algos: List<Algo> = emptyList()
-    val errorNombre: String = "",
-    val errorTipo: String = "",
+    val nombre: String? = "",
+    val tipo: String? = "",
+    val algos: List<AlgoEntity> = emptyList(),
+    val errorNombre: String? = "",
+    val errorTipo: String? = "",
     val isSuccess: Boolean = false
 )
