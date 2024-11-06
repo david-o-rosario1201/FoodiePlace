a2bdb46927bfdb4bd57d64f2ae0f592e388f7e0f
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-06 10:34:21 -0600

    Crea ProductoDTO

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/ProductoDto.kt
@@ -0,0 +1,13 @@
+package edu.ucne.proyectofinalaplicada2.data.remote.dto
+
+import java.math.BigDecimal
+
+data class ProductoDto (
+    val productoId: Int,
+    val nombre: String,
+    val categoriaId: Int,
+    val descripcion: String,
+    val precio: BigDecimal,
+    val disponibilidad: Boolean,
+    val imagen: String
+)
