8b1ea16caec227b74c6039d0b44146d52a872143
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-08 11:46:17 -0400

    Eliminar componente de prueba

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -0,0 +1,15 @@
+package edu.ucne.proyectofinalaplicada2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+//@Database(
+//    entities = [
+//        ,
+//    ],
+//    version = 1,
+//    exportSchema = false
+//)
+
+abstract class FoodiePlaceDb : RoomDatabase(){
+}
