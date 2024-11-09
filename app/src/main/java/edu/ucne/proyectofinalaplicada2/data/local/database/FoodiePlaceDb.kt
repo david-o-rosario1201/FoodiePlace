a6622b010e4d662fb0788f1a70d5d6bc711915e7
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-09 02:41:53 -0400

    Agrega UsuarioEntity y UsuarioDao a la base de datos

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -2,14 +2,15 @@ package edu.ucne.proyectofinalaplicada2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.proyectofinalaplicada2.data.local.dao.UsuarioDao
+import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
 
-//@Database(
-//    entities = [
-//        ,
-//    ],
-//    version = 1,
-//    exportSchema = false
-//)
+@Database(
+    version = 1,
+    exportSchema = false,
+    entities = [UsuarioEntity::class]
+)
 
 abstract class FoodiePlaceDb : RoomDatabase(){
+    abstract fun usuarioDao(): UsuarioDao
 }
