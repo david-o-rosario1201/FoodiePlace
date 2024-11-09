10cff8872bb21df08d288617a0fdef4e4df5fa1c
HenzoWTF <Urenahenzo@gmail.com>
2024-11-09 00:58:52 -0400

    Cambia tipo Date a String de FechaResena en ReseñasDTO

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/ReseñasDTO.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/ReseñasDTO.kt
@@ -1,11 +1,9 @@
 package edu.ucne.proyectofinalaplicada2.data.remote.dto
 
-import java.util.Date
-
-data class ReseñasDTO (
+data class ReseñasDTO(
     val resenaId: Int,
     val usuarioId: Int,
     val comentario: String,
-    val fechaResena: Date,
+    val fechaResena: String,
     val calificacion: Int,
 )
