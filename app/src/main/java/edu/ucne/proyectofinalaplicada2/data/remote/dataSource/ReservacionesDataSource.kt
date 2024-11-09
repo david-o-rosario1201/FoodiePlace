d4b0588c4d7864584c232fb3e6506468cecc456f
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-09 17:13:40 -0600

    Modifica ReservacionesDataSource

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/ReservacionesDataSource.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/ReservacionesDataSource.kt
@@ -2,14 +2,19 @@ package edu.ucne.proyectofinalaplicada2.data.remote.dataSource
 
 import edu.ucne.proyectofinalaplicada2.data.local.dto.ReservacionesDto
 import edu.ucne.proyectofinalaplicada2.data.remote.API.ReservacionesAPI
+
 import javax.inject.Inject
 
-class ReservacionesDataSource @Inject constructor(
-    private val reservacionesAPI: ReservacionesAPI
-){
-        suspend fun getReservaciones() = reservacionesAPI.getReservaciones()
-        suspend fun getReservacionById(id: Int) = reservacionesAPI.getReservacionById(id)
-        suspend fun postReservacion(reservacion: ReservacionesDto) = reservacionesAPI.postReservacion(reservacion)
-        suspend fun  putReservacion(id: Int, reservacion: ReservacionesDto) = reservacionesAPI.putReservacion(id, reservacion)
-        suspend fun deleteReservacion(id: Int) = reservacionesAPI.deleteReservacion(id)
+class ReservacionesRemoteDataSource @Inject constructor(
+        private val reservacionesApi: ReservacionesAPI
+) {
+        suspend fun getReservaciones() = reservacionesApi.getReservaciones()
+
+        suspend fun getReservacionById(id: Int) = reservacionesApi.getReservacionById(id)
+
+        suspend fun addReservacion(reservacion: ReservacionesDto) = reservacionesApi.postReservacion(reservacion)
+
+        suspend fun updateReservacion(id: Int, reservacion: ReservacionesDto) = reservacionesApi.putReservacion(id, reservacion)
+
+        suspend fun deleteReservacion(id: Int) = reservacionesApi.deleteReservacion(id)
 }
