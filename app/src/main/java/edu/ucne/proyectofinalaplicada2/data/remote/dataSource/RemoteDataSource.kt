44ab752ed1cee6e2114c0441b99821f2c86d4f4a
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-09 02:44:53 -0400

    Agrega los métodos de Usuarios en la class RemoteDataSource

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/RemoteDataSource.kt
@@ -1,10 +1,20 @@
 package edu.ucne.proyectofinalaplicada2.data.remote.dataSource
 
 import edu.ucne.proyectofinalaplicada2.data.remote.API.FoodiePlaceApi
+import edu.ucne.proyectofinalaplicada2.data.remote.dto.UsuarioDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val foodiePlaceApi: FoodiePlaceApi
 ) {
+    //Usuarios
+    suspend fun addUsuario(usuarioDto: UsuarioDto) = foodiePlaceApi.addUsuario(usuarioDto)
 
+    suspend fun getUsuario(usuarioId: Int) = foodiePlaceApi.getUsuario(usuarioId)
+
+    suspend fun deleteUsuario(usuarioId: Int) = foodiePlaceApi.deleteUsuario(usuarioId)
+
+    suspend fun updateUsuario(usuarioId: Int, usuario: UsuarioDto) = foodiePlaceApi.updateUsuario(usuarioId,usuario)
+
+    suspend fun getUsuarios() = foodiePlaceApi.getUsuarios()
 }
