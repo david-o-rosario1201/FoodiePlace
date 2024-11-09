cebc015feb98979fe552aa1ed43c62c25e96faba
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-09 02:36:34 -0400

    Crea la interface UsuarioDao

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/dao/UsuarioDao.kt
@@ -0,0 +1,27 @@
+package edu.ucne.proyectofinalaplicada2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UsuarioDao {
+    @Upsert()
+    suspend fun addUsuario(usuario: UsuarioEntity)
+
+    @Query("""
+        SELECT * FROM Usuarios
+        WHERE usuarioId = :usuarioId
+        LIMIT 1
+    """)
+    suspend fun getUsuario(usuarioId: Int): UsuarioEntity?
+
+    @Delete
+    suspend fun deleteUsuario(usuario: UsuarioEntity)
+
+    @Query("SELECT * FROM Usuarios")
+    fun getUsuarios(): Flow<List<UsuarioEntity>>
+}
