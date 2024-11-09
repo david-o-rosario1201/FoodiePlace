5dfd48a5eb732272c6165707496f37baaa77cd36
HenzoWTF <Urenahenzo@gmail.com>
2024-11-09 14:39:42 -0400

    Crea PagosRemoteDataSource

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/PagosRemoteDataSource.kt
@@ -0,0 +1,18 @@
+package edu.ucne.proyectofinalaplicada2.data.remote.dataSource
+
+import edu.ucne.proyectofinalaplicada2.data.remote.API.PagosAPI
+import edu.ucne.proyectofinalaplicada2.data.remote.API.ReseñasAPI
+import edu.ucne.proyectofinalaplicada2.data.remote.dto.PagosDTO
+import edu.ucne.proyectofinalaplicada2.data.remote.dto.ReseñasDTO
+import javax.inject.Inject
+
+class PagosRemoteDataSource @Inject constructor(
+    private val PagosAPI: PagosAPI
+) {
+    suspend fun postPago(pago: PagosDTO) = PagosAPI.postPago(pago)
+    suspend fun getPagoById(id: Int) = PagosAPI.getPagoById(id)
+    suspend fun deletePago(id: Int) = PagosAPI.deletePago(id)
+    suspend fun putPago(id: Int, pago: PagosDTO) = PagosAPI.putPago(id, pago)
+    suspend fun getPagos() = PagosAPI.getPagos()
+
+}
