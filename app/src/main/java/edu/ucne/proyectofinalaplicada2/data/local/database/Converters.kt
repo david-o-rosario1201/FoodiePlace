912d5be9527354f2ab027f99cb77dcb5d59f778e
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-10 13:48:48 -0400

    Crea la class Converters

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/Converters.kt
@@ -0,0 +1,29 @@
+package edu.ucne.proyectofinalaplicada2.data.local.database
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let {
+            Date(it)
+        }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun fromString(value: String?): BigDecimal? {
+        return value?.let { BigDecimal(it) }
+    }
+
+    @TypeConverter
+    fun bigDecimalToString(bigDecimal: BigDecimal?): String? {
+        return bigDecimal?.toPlainString()
+    }
+}
