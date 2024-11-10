5758a7246819833fa73fabb61d9625217dbb3a88
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 01:47:56 -0400

    Crea la CategoriaRepository

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/repository/CategoriaRepository.kt
@@ -0,0 +1,54 @@
+package edu.ucne.proyectofinalaplicada2.data.repository
+
+import edu.ucne.proyectofinalaplicada2.data.local.dao.CategoriaDao
+import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
+import edu.ucne.proyectofinalaplicada2.data.remote.Resource
+import edu.ucne.proyectofinalaplicada2.data.remote.dataSource.CategoriaRemoteDataSource
+import edu.ucne.proyectofinalaplicada2.data.remote.dto.CategoriaDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class CategoriaRepository @Inject constructor(
+    private val categoriaRemoteDataSource: CategoriaRemoteDataSource,
+    private val categoriaDao: CategoriaDao
+) {
+    suspend fun getCategoriaById(id: Int) = categoriaRemoteDataSource.getCategoriaById(id)
+    suspend fun getCategoria() = categoriaRemoteDataSource.getCategorias()
+    suspend fun addCategoria(categoria: CategoriaDto) = categoriaRemoteDataSource.postCategoria(categoria)
+    suspend fun updateCategoria(id: Int, categoria: CategoriaDto) = categoriaRemoteDataSource.putCategoria(id, categoria)
+    suspend fun deleteCategoria(id: Int) = categoriaRemoteDataSource.deleteCategoria(id)
+
+    fun getCategorias(): Flow<Resource<List<CategoriaEntity>>> = flow {
+        try{
+            emit(Resource.Loading())
+            val categorias = categoriaRemoteDataSource.getCategorias()
+
+            categorias.forEach {
+                categoriaDao.save(
+                    it.toCategoriaEntity()
+                )
+            }
+
+            categoriaDao.getAll().collect{categoriasLocal ->
+                emit(Resource.Success(categoriasLocal))
+            }
+
+        }catch (e: HttpException){
+            emit(Resource.Error(e.message ?: "Error HTTP GENERAL"))
+        }catch (e: Exception){
+            emit(Resource.Error(e.message ?: "Verificar conexion a internet"))
+
+            categoriaDao.getAll().collect{categoriasLocal ->
+                emit(Resource.Success(categoriasLocal))
+            }
+        }
+    }
+}
+
+private fun CategoriaDto.toCategoriaEntity() = CategoriaEntity(
+    categoriaId = categoriaId,
+    nombre = nombre,
+    imagen = imagen
+)
