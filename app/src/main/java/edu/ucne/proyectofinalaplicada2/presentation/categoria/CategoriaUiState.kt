d0e5ac0861faf1c1e72e163187cfd5e85a0ecaee
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 01:48:15 -0400

    Crea la CategoriaUiState

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/categoria/CategoriaUiState.kt
@@ -0,0 +1,14 @@
+package edu.ucne.proyectofinalaplicada2.presentation.categoria
+
+import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
+
+data class CategoriaUiState (
+    val categoriaId: Int? = null,
+    val nombre: String = "",
+    val imagen: String = "",
+    val errorMessge: String? = null,
+    val success: Boolean = false,
+    val isLoading: Boolean = false,
+    val categorias: List<CategoriaEntity> = emptyList(),
+    val isRefreshing: Boolean =false
+)
