d252f822905c937b74c73fa7cbd382c3469d314c
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-09 23:04:47 -0600

    Closed #19

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/ProductoEntity.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/ProductoEntity.kt
@@ -1,10 +1,22 @@
 package edu.ucne.proyectofinalaplicada2.data.local.entities
 
 import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.math.BigDecimal
 
-@Entity(tableName = "Productos")
+@Entity(
+    tableName = "Productos"
+            foreignKeys = [
+        ForeignKey(
+            entity = CategoriasEntity::class,
+            parentColumns = ["categoriaId"],
+            childColumns = ["categoriaId"]
+        )
+    ],
+    indices = [Index("categoriaId")]
+)
 data class ProductoEntity(
     @PrimaryKey(autoGenerate = true)
     val productoId: Int,
