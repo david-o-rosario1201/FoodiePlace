5c31ceb066072cca9f1c9d959ae7c4963073b6ad
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 02:50:36 -0400

    Elimina Delete de PagosUIEvent

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUIEvent.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUIEvent.kt
@@ -3,6 +3,5 @@ package edu.ucne.proyectofinalaplicada2.presentation.pagos
 sealed interface PagosUIEvent {
     data class IsRefreshingChanged(val isRefreshing: Boolean): PagosUIEvent
     object Save: PagosUIEvent
-    object Delete: PagosUIEvent
     data object Refresh: PagosUIEvent
 }
