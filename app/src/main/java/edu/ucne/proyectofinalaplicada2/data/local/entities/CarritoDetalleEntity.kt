c8b446a160cd4e10df2440c786cdb096e67ec36d
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 10:59:05 -0400

    Agrega PrimaryKey a CarritoDetalleEntity

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/CarritoDetalleEntity.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/CarritoDetalleEntity.kt
@@ -1,8 +1,12 @@
 package edu.ucne.proyectofinalaplicada2.data.local.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.math.BigDecimal
 
+@Entity(tableName = "CarritoDetalle")
 data class CarritoDetalleEntity (
+    @PrimaryKey
     val carritoDetalleId : Int,
     val carritoId : Int,
     val productoId : Int,
