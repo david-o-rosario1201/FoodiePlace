8591798f4df7447712896b769a19ae79d0b1cd53
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 02:12:35 -0400

    Modifica cambio de Class a Interface PagosUIEvent

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUIEvent.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUIEvent.kt
@@ -1,9 +1,8 @@
 package edu.ucne.proyectofinalaplicada2.presentation.pagos
 
-sealed class PagosUIEvent {
-    data class IsRefreshingChanged(val isRefreshing: Boolean): PagosUIEvent()
-    object Save: PagosUIEvent()
-    object Delete: PagosUIEvent()
-    data object Refresh: PagosUIEvent()
-
+sealed interface PagosUIEvent {
+    data class IsRefreshingChanged(val isRefreshing: Boolean): PagosUIEvent
+    object Save: PagosUIEvent
+    object Delete: PagosUIEvent
+    data object Refresh: PagosUIEvent
 }
