b133e0aed8a31ff9f6deb359b539059afc1306f2
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-10 14:00:34 -0600

    Crea TarjetaUiState

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/tarjeta/TarjetaUiState.kt
@@ -0,0 +1,16 @@
+package edu.ucne.proyectofinalaplicada2.presentation.tarjeta
+
+import edu.ucne.proyectofinalaplicada2.data.remote.dto.TarjetaDto
+
+data class TarjetaUiState(
+    val tarjetaId: Int? = null,
+    val usuarioId: Int? = null,
+    val tipoTarjeta: String? = "",
+    val numeroTarjeta: String? = "",
+    val fechaExpiracion: String? = "",
+    val cvv: String? = "",
+    val tarjetas: List<TarjetaDto> = emptyList(),
+    val isLoading: Boolean = false,
+    val success: Boolean = false,
+    val isRefreshing: Boolean = false
+)
