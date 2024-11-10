1bbb96fc4a42439e6e84b4ac7b9fbb47ce3d12ea
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-10 02:46:15 -0400

    Agrega OfertaEntity y OfertaDao a la base de datos

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -2,15 +2,18 @@ package edu.ucne.proyectofinalaplicada2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.proyectofinalaplicada2.data.local.dao.OfertaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ReviewDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.UsuarioDao
+import edu.ucne.proyectofinalaplicada2.data.local.entities.OfertaEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.ReviewEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
 
 @Database(
     entities = [
         ReviewEntity::class,
-        UsuarioEntity::class
+        UsuarioEntity::class,
+        OfertaEntity::class
     ],
     version = 1,
     exportSchema = false
@@ -19,4 +22,5 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
 abstract class FoodiePlaceDb : RoomDatabase(){
     abstract fun reviewDao(): ReviewDao
     abstract fun usuarioDao(): UsuarioDao
+    abstract fun ofertaDao(): OfertaDao
 }
