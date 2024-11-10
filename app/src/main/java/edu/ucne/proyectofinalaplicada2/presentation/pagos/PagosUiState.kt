7cb066c74e600b5fa251a0857afd9e7515a61b1f
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 02:51:01 -0400

    Cambia BigDecimal por Double en PagosUiState

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUiState.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pagos/PagosUiState.kt
@@ -1,12 +1,14 @@
 package edu.ucne.proyectofinalaplicada2.presentation.pagos
 
 import edu.ucne.proyectofinalaplicada2.data.local.entities.PagosEntity
+import java.math.BigDecimal
 
 data class PagosUiState (
     val id: Int? = null,
-    val usuarioId: Int = 0,
+    val pedidoId: Int = 0,
+    val tarjetaId: Int = 0,
     val fechaPago: String = "",
-    val monto: Double = 0.0,
+    val monto: BigDecimal = BigDecimal.ZERO,
     val errorMessge: String? = null,
     val success: Boolean = false,
     val isLoading: Boolean = false,
