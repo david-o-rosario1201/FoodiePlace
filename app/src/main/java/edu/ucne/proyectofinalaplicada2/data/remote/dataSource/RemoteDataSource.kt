86106f12543098664fdad157da27dc59ae3f97c4
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-09 23:21:19 -0400

    Crea UsuarioRemoteDataSource

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dataSource/RemoteDataSource.kt
@@ -7,14 +7,5 @@ import javax.inject.Inject
 class RemoteDataSource @Inject constructor(
     private val foodiePlaceApi: FoodiePlaceApi
 ) {
-    //Usuarios
-    suspend fun addUsuario(usuarioDto: UsuarioDto) = foodiePlaceApi.addUsuario(usuarioDto)
 
-    suspend fun getUsuario(usuarioId: Int) = foodiePlaceApi.getUsuario(usuarioId)
-
-    suspend fun deleteUsuario(usuarioId: Int) = foodiePlaceApi.deleteUsuario(usuarioId)
-
-    suspend fun updateUsuario(usuarioId: Int, usuario: UsuarioDto) = foodiePlaceApi.updateUsuario(usuarioId,usuario)
-
-    suspend fun getUsuarios() = foodiePlaceApi.getUsuarios()
 }
