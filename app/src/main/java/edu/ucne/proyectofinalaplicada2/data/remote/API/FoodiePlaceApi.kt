0d310fc13b405ae13cc4fe7dd1db06481ca1647d
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-09 23:15:06 -0400

    Crea UsuarioApi

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/API/FoodiePlaceApi.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/API/FoodiePlaceApi.kt
@@ -1,36 +1,5 @@
 package edu.ucne.proyectofinalaplicada2.data.remote.API
 
-import edu.ucne.proyectofinalaplicada2.data.remote.dto.UsuarioDto
-import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Path
-
 interface FoodiePlaceApi {
-    @Headers("X-Api-Key:kaisokuni_orewanara")
-    @POST("api/Usuarios")
-    suspend fun addUsuario(@Body usuarioDto: UsuarioDto): UsuarioDto
 
-    @Headers("X-Api-Key:kaisokuni_orewanara")
-    @GET("api/Usuarios/{usuarioId}")
-    suspend fun getUsuario(@Path("usuarioId") usuarioId: Int): UsuarioDto
-
-    @Headers("X-Api-Key:kaisokuni_orewanara")
-    @DELETE("api/Usuarios/{usuarioId}")
-    suspend fun deleteUsuario(@Path("usuarioId") usuarioId: Int): Response<Unit>
-
-    @Headers("X-Api-Key:kaisokuni_orewanara")
-    @PUT("api/Usuarios/{usuarioId}")
-    suspend fun updateUsuario(
-        @Path("usuarioId") usuarioId: Int,
-        @Body usuario: UsuarioDto
-    ): Response<UsuarioDto>
-
-    @Headers("X-Api-Key:kaisokuni_orewanara")
-    @GET("api/Usuarios")
-    suspend fun getUsuarios(): List<UsuarioDto>
 }
--- app/src/main/java/edu/ucne/proyectofinalaplicada2/di/AppModule.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/di/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.proyectofinalaplicada2.data.local.database.FoodiePlaceDb
 import edu.ucne.proyectofinalaplicada2.data.remote.API.FoodiePlaceApi
+import edu.ucne.proyectofinalaplicada2.data.remote.API.UsuarioApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -33,6 +34,16 @@ object AppModule {
             .build()
             .create(FoodiePlaceApi::class.java)
         }
+
+    @Provides
+    @Singleton
+    fun providesUsuarioApi(moshi: Moshi): UsuarioApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(UsuarioApi::class.java)
+    }
 
     @Provides
     @Singleton
