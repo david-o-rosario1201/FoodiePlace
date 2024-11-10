2cad93a58bc40fb7c1b83cf9d6446212256f6807
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-10 02:46:15 -0400

    Agrega OfertaEntity y OfertaDao a la base de datos

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -2,6 +2,7 @@ package edu.ucne.proyectofinalaplicada2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.proyectofinalaplicada2.data.local.dao.OfertaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CategoriaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ReservacionesDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ReviewDao
@@ -10,6 +11,7 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.ReservacionesEntity
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDetalleDao
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
+import edu.ucne.proyectofinalaplicada2.data.local.entities.OfertaEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.ReviewEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoEntity
@@ -18,6 +20,8 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoDetalleEntity
 @Database(
     entities = [
         ReviewEntity::class,
+        UsuarioEntity::class,
+        OfertaEntity::class
         UsuarioEntity::class,
         ReservacionesEntity::class,
         CategoriaEntity::class,
@@ -36,5 +40,5 @@ abstract class FoodiePlaceDb : RoomDatabase(){
     abstract fun categoriaDao(): CategoriaDao
     abstract fun carritoDao(): CarritoDao
     abstract fun carritoDetalleDao(): CarritoDetalleDao
-
+    abstract fun ofertaDao(): OfertaDao
 }
