e8af283d6856453a5f89b4e29bfe749f4bc128ac
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-10 02:46:15 -0400

    Agrega OfertaEntity y OfertaDao a la base de datos

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -2,6 +2,7 @@ package edu.ucne.proyectofinalaplicada2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.proyectofinalaplicada2.data.local.dao.OfertaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CategoriaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ReservacionesDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ProductoDao
@@ -11,6 +12,7 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.ReservacionesEntity
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDetalleDao
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
+import edu.ucne.proyectofinalaplicada2.data.local.entities.OfertaEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.ReviewEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.UsuarioEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoEntity
@@ -21,13 +23,12 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.ProductoEntity
     entities = [
         ReviewEntity::class,
         UsuarioEntity::class,
-        UsuarioEntity::class,
+        OfertaEntity::class,
         ProductoEntity::class,
         ReservacionesEntity::class,
         CategoriaEntity::class,
         CarritoEntity::class,
         CarritoDetalleEntity::class,
-
     ],
     version = 1,
     exportSchema = false
@@ -42,5 +43,5 @@ abstract class FoodiePlaceDb : RoomDatabase(){
     abstract fun categoriaDao(): CategoriaDao
     abstract fun carritoDao(): CarritoDao
     abstract fun carritoDetalleDao(): CarritoDetalleDao
-
+    abstract fun ofertaDao(): OfertaDao
 }
