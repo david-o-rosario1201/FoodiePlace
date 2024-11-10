b186123aad25ea403f58c92a8b919457b72baab6
HenzoWTF <Urenahenzo@gmail.com>
2024-11-10 01:58:51 -0400

    Elimina import categoritaDao Repetida en la Bd

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/database/FoodiePlaceDb.kt
@@ -3,7 +3,6 @@ package edu.ucne.proyectofinalaplicada2.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CategoriaDao
-import edu.ucne.proyectofinalaplicada2.data.local.dao.CategoriaDao
 import edu.ucne.proyectofinalaplicada2.data.local.dao.ReservacionesDao
 
 
