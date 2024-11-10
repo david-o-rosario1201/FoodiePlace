5a76ff354ee561cf244c8d9faa08bccd30b6e548
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-10 13:34:41 -0600

    Crea Tarjeta Dao

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/dao/TarjetaDao.kt
@@ -0,0 +1,31 @@
+package edu.ucne.proyectofinalaplicada2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.proyectofinalaplicada2.data.local.entities.TarjetaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TarjetaDao {
+    @Upsert
+    suspend fun addTarjeta(tarjeta: TarjetaEntity)
+
+    @Query("""
+        SELECT * 
+        FROM Tarjetas
+        WHERE tarjetaId=:id
+        LIMIT 1
+    """)
+    suspend fun getTarjetaById(id: Int): TarjetaEntity?
+
+    @Delete
+    suspend fun deleteTarjeta(tarjeta: TarjetaEntity)
+
+    @Query("""
+        SELECT *
+        FROM Tarjetas
+    """)
+    fun getTarjetas(): Flow<List<TarjetaEntity>>
+}
