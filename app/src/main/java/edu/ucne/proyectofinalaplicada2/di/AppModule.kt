484a87b0323247a1123be384aaa32114ab5990c2
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-09 22:46:51 -0600

    Agrega Producto a AppModule

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/di/AppModule.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/di/AppModule.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.proyectofinalaplicada2.data.local.database.FoodiePlaceDb
 import edu.ucne.proyectofinalaplicada2.data.remote.API.FoodiePlaceApi
+import edu.ucne.proyectofinalaplicada2.data.remote.API.ProductoApi
 import edu.ucne.proyectofinalaplicada2.data.remote.API.ReseñasAPI
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -57,4 +58,18 @@ object AppModule {
             .build()
             .create(ReseñasAPI::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun ProvidesProductoApi(moshi: Moshi): ProductoApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ProductoApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun providesProductoDao(db: FoodiePlaceDb) = db.ProductoDao
+
 }
