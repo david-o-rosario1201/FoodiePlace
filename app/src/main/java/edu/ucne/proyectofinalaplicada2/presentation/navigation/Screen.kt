63eafd5f11631653e1daef51911bd2d8ae782e37
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-12 13:15:01 -0400

    Agrega las rutas HomeListScreen, CarritoListScreen, PedidoListScreen y RewviewListScreen a la selead class Screen

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/Screen.kt
@@ -3,6 +3,19 @@ package edu.ucne.proyectofinalaplicada2.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
+
+    @Serializable
+    data object HomeScreen: Screen()
+
+    @Serializable
+    data object CarritoListScreen: Screen()
+
+    @Serializable
+    data object PedidoListScreen: Screen()
+
+    @Serializable
+    data object ReviewListScreen: Screen()
+
     @Serializable
     data object UsuarioListScreen: Screen()
 
