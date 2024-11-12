e00789d1a6af99eb903c1ab006b759ec59f04583
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-12 16:08:47 -0600

    modifica NavHost

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/ProyectoFinalAplicada2NavHost.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/ProyectoFinalAplicada2NavHost.kt
@@ -1,7 +1,27 @@
 package edu.ucne.proyectofinalaplicada2.presentation.navigation
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
-fun ProyectoFinalAplicada2NavHost() {
+fun ProyectoFinalAplicada2NavHost(
+    navHostController: NavHostController
+) {
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    DrawerMenu(
+        drawerState = drawerState,
+        navHostController = navHostController
+    ) {
+        NavHost(
+            navController = navHostController,
+            startDestination = Screen.WelcomeScreen
+        ) {
+
+        }
+    }
 }
