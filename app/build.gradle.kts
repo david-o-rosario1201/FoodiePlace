ea19a6ae68e1bab680e01cf9d6a76a38f08976ee
HenzoWTF <Urenahenzo@gmail.com>
2024-11-13 19:17:07 -0400

    Coloca las librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.adapters)
     implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.androidx.foundation.layout.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
