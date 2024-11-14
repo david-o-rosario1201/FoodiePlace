0d37fdfb4e4033b52dbeed6c7a02d14beedc431a
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-13 20:11:13 -0600

    Crea producto list screen

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/producto/ProductoListScreen.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/producto/ProductoListScreen.kt
@@ -1,6 +1,5 @@
 package edu.ucne.proyectofinalaplicada2.presentation.producto
 
-import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
