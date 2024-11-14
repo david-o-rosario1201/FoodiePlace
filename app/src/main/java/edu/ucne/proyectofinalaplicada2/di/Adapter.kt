c7d2048c6e1bdf273835b1bfebc2b8681fea857e
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-13 23:11:53 -0600

    Modifica y agrega adapter para decimal

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/di/Adapter.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/di/Adapter.kt
@@ -2,11 +2,12 @@ package edu.ucne.proyectofinalaplicada2.di
 
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class DateAdapter {
+class Adapter {
     private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
 
     @ToJson
@@ -17,5 +18,15 @@ class DateAdapter {
     @FromJson
     fun fromJson(value: String?): Date? {
         return value?.let { dateFormat.parse(it) }
+    }
+
+    @ToJson
+    fun toJson(value: BigDecimal): String {
+        return value.toString()
+    }
+
+    @FromJson
+    fun fromJson(value: String): BigDecimal {
+        return BigDecimal(value)
     }
 }
