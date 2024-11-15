bd367b74a66d06dc6cc72b70fc6be55174e22a4b
HenzoWTF <Urenahenzo@gmail.com>
2024-11-14 20:54:59 -0400

    Modifica la variable imagen String a ByteArray para la imagen en el Entity

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/CategoriaEntity.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/local/entities/CategoriaEntity.kt
@@ -6,9 +6,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 @Entity(tableName = "Categorias")
-data class CategoriaEntity (
+data class CategoriaEntity(
     @PrimaryKey
     val categoriaId: Int,
     val nombre: String,
-    val imagen: String
+    val imagen: ByteArray
 )
