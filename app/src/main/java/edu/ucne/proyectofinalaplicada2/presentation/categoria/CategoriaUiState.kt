02f173d788434bbc708c05b2504451360ecde0f2
HenzoWTF <Urenahenzo@gmail.com>
2024-11-14 20:57:02 -0400

    Agrega SetNombreError pero con planes de modificar

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/categoria/CategoriaUiState.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/categoria/CategoriaUiState.kt
@@ -5,10 +5,12 @@ import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
 data class CategoriaUiState (
     val categoriaId: Int? = null,
     val nombre: String = "",
-    val imagen: String = "",
+    val imagen: ByteArray? = null,
+    val nombreError: String? = null,
+    val imagenError: String? = null,
     val errorMessge: String? = null,
     val success: Boolean = false,
     val isLoading: Boolean = false,
     val categorias: List<CategoriaEntity> = emptyList(),
-    val isRefreshing: Boolean =false
+    val isRefreshing: Boolean = false
 )
