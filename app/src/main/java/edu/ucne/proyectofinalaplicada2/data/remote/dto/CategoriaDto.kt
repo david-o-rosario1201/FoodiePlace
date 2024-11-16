a4325c819b74388e1ee08354c870e14d9df80fec
HenzoWTF <Urenahenzo@gmail.com>
2024-11-15 22:37:15 -0400

    Modifica el categoriaId para que acepte null

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/CategoriaDto.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/CategoriaDto.kt
@@ -1,7 +1,7 @@
 package edu.ucne.proyectofinalaplicada2.data.remote.dto
 
 data class CategoriaDto (
-    val categoriaId: Int,
+    val categoriaId: Int?,
     val nombre: String,
     val imagen: ByteArray?
 )
