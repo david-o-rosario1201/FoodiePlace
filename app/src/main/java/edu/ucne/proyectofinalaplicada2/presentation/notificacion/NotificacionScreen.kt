0855505df69c93d987d31a3b2be7152d32c96ca1
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-17 00:08:20 -0400

    Crea NoficacionScreen

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/notificacion/NotificacionScreen.kt
@@ -0,0 +1,128 @@
+package edu.ucne.proyectofinalaplicada2.presentation.notificacion
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.proyectofinalaplicada2.R
+import edu.ucne.proyectofinalaplicada2.presentation.components.SimpleTopBarComponent
+import edu.ucne.proyectofinalaplicada2.ui.theme.ProyectoFinalAplicada2Theme
+import edu.ucne.proyectofinalaplicada2.ui.theme.sampleNotificacionUiState
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Composable
+fun NotificacionScreen(
+    viewModel: NotificacionViewModel = hiltViewModel(),
+    goToHome: () -> Unit
+){
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    NotificacionBodyScreen(
+        uiState = uiState,
+        goToHome = goToHome
+    )
+}
+
+@Composable
+fun NotificacionBodyScreen(
+    uiState: NotificacionUiState,
+    goToHome: () -> Unit
+) {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+    Scaffold(
+        topBar = {
+            SimpleTopBarComponent(
+                title = "Notificaciones",
+                onBackClick = goToHome
+            )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            items(uiState.notificaciones) { uiState ->
+                Card(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 5.dp)
+                        .fillMaxWidth(),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White,
+                        contentColor = Color.White
+                    ),
+                    elevation = CardDefaults.cardElevation(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.logo),
+                            contentDescription = "Logo FoodiePlace",
+                            modifier = Modifier.size(50.dp)
+                        )
+                        Column(
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(start = 10.dp)
+                        ) {
+                            Text(
+                                text = "FoodiePlace",
+                                color = Color.Black,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                            Text(
+                                text = uiState.descripcion,
+                                color = Color.Gray,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                        Text(
+                            text = uiState.fecha.let { it -> dateFormat.format(it) } ?: "",
+                            color = Color.Gray,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun NotificacionBodyScreenPreview(){
+    ProyectoFinalAplicada2Theme {
+        NotificacionBodyScreen(
+            uiState = sampleNotificacionUiState,
+            goToHome = {}
+        )
+    }
+}
