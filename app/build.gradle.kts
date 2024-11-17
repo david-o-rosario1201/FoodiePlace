e1301995baf7ed5a789c7b8471a3282c6f7b2246
HenzoWTF <Urenahenzo@gmail.com>
2024-11-17 00:34:52 -0400

    Creacion del HomeViewModel para poder manejar mejor los datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.volley)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
