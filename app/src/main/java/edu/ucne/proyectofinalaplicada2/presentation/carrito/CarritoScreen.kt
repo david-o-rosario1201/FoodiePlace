48f9861b6790596d6f2dd58dcee0a0d7fae65428
HenzoWTF <Urenahenzo@gmail.com>
2024-11-18 00:03:30 -0400

    Creacion de CarritoScreen

---
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/carrito/CarritoScreen.kt
@@ -0,0 +1,11 @@
+package edu.ucne.proyectofinalaplicada2.presentation.carrito
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CarritoScreen(
+    viewModel: CarritoViewModel = hiltViewModel(),
+    goToCarritoList: () -> Unit
+){
+}
