f64e1b4fdef3e29bf9091dd7219ed3f60f8e8833
HenzoWTF <Urenahenzo@gmail.com>
2024-11-18 00:16:18 -0400

    Crea la funcion CarritoBodyScreen

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/carrito/CarritoScreen.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/carrito/CarritoScreen.kt
@@ -1,11 +1,61 @@
 package edu.ucne.proyectofinalaplicada2.presentation.carrito
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import edu.ucne.proyectofinalaplicada2.presentation.components.TopBarComponent
+import edu.ucne.proyectofinalaplicada2.presentation.navigation.BottomBarNavigation
+import java.lang.reflect.Modifier
 
 @Composable
 fun CarritoScreen(
     viewModel: CarritoViewModel = hiltViewModel(),
+    navController: NavHostController,
     goToCarritoList: () -> Unit
 ){
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    CarritoBodyScreen(
+        uiState = uiState,
+        goToCarritoList = goToCarritoList,
+        navController = navController
+    )
+}
+
+@Composable
+private fun CarritoBodyScreen(
+    uiState: CarritoUiState,
+    goToCarritoList: () -> Unit,
+    navController: NavHostController,
+) {
+    Scaffold(
+        topBar = {
+            TopBarComponent(
+                title = "Carrito",
+                onClickMenu = {},
+                onClickNotifications = {},
+                notificationCount = 0
+            )
+        },
+        bottomBar = {
+            BottomBarNavigation(
+                navController = navController
+            )
+        }
+    ){paddingValues ->
+        Column(
+            modifier = androidx.compose.ui.Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+
+        }
+    }
 }
