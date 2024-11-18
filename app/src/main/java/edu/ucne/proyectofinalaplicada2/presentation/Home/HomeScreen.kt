cfdccf92c9c9826c1c94c6de7eb6f0bcf5cf320d
HenzoWTF <Urenahenzo@gmail.com>
2024-11-17 22:41:30 -0400

    Modificacion de UI HomeScreen

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/Home/HomeScreen.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/Home/HomeScreen.kt
@@ -7,17 +7,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,7 +36,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CategoriaEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.ProductoEntity
 import edu.ucne.proyectofinalaplicada2.presentation.categoria.CategoriaUiState
@@ -59,6 +65,7 @@ fun HomeScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeBodyScreen(
     uiState: HomeUiState,
@@ -89,7 +96,8 @@ fun HomeBodyScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it),
+                .padding(it)
+                .padding(horizontal = 16.dp),
         ) {
             Spacer(modifier = Modifier.height(16.dp))
             Text(
@@ -107,17 +115,36 @@ fun HomeBodyScreen(
                     color = Color.Black,
                 )
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
             // Actualizar el SearchBar con el query
-            OutlinedTextField(
+            TextField(
                 value = searchQuery,
                 onValueChange = { query ->
                     searchQuery = query
                     onSearchQueryChanged(query)
                 },
                 label = { Text("Buscar producto") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = Color.Gray,
+                        modifier = Modifier.size(30.dp)
+                    )
+                },
+                colors = androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = Color.Gray.copy(alpha = 0.2f),
+                    focusedBorderColor = Color.Gray,
+                    unfocusedBorderColor = Color.Gray,
+                    focusedLabelColor = Color.Gray,
+                    unfocusedLabelColor = Color.Gray
+                ),
+                shape = RoundedCornerShape(15.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp)
+                    .align(Alignment.CenterHorizontally)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
