92c7a975d696a54b8148acbf1b3a88feb2f3c768
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-19 00:46:23 -0400

    Arregla componente BottomBarNavigation para que muestre el ícono seleccionado cuando este en su ruta

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/BottomBarNavigation.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/BottomBarNavigation.kt
@@ -2,7 +2,6 @@ package edu.ucne.proyectofinalaplicada2.presentation.navigation
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
@@ -20,14 +19,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import edu.ucne.proyectofinalaplicada2.ui.theme.ProyectoFinalAplicada2Theme
 import edu.ucne.proyectofinalaplicada2.ui.theme.color_oro
 
@@ -35,7 +33,43 @@ import edu.ucne.proyectofinalaplicada2.ui.theme.color_oro
 fun BottomBarNavigation(
     navController: NavHostController
 ) {
-    var selectedItemIndex by remember { mutableStateOf("Home") }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+            badgeCount = 0,
+            route = "edu.ucne.proyectofinalaplicada2.presentation.navigation.Screen.HomeScreen"
+        ),
+        BottomNavigationItem(
+            title = "Carrito",
+            selectedIcon = Icons.Filled.ShoppingCart,
+            unselectedIcon = Icons.Outlined.ShoppingCart,
+            hasNews = false,
+            badgeCount = 0,
+            route = "edu.ucne.proyectofinalaplicada2.presentation.navigation.Screen.CarritoListScreen"
+        ),
+        BottomNavigationItem(
+            title = "Pedidos",
+            selectedIcon = Icons.Filled.DateRange,
+            unselectedIcon = Icons.Outlined.DateRange,
+            hasNews = false,
+            badgeCount = 0,
+            route = "edu.ucne.proyectofinalaplicada2.presentation.navigation.Screen.PedidoListScreen"
+        ),
+        BottomNavigationItem(
+            title = "Reviews",
+            selectedIcon = Icons.Filled.Star,
+            unselectedIcon = Icons.Outlined.Star,
+            hasNews = false,
+            badgeCount = 0,
+            route = "edu.ucne.proyectofinalaplicada2.presentation.navigation.Screen.ReviewListScreen"
+        )
+    )
 
     Surface(
         shape = RoundedCornerShape(
@@ -43,75 +77,40 @@ fun BottomBarNavigation(
             topEnd = 30.dp
         ),
         color = color_oro,
-        modifier = Modifier.height(90.dp)
+        modifier = Modifier.height(120.dp)
     ) {
         NavigationBar(
             containerColor = color_oro,
             modifier = Modifier.fillMaxWidth()
         ) {
-            NavigationBarItem(
-                label = { Text(text = "Home") },
-                icon = {
-                    Icon(
-                        imageVector = if (selectedItemIndex == "Home") Icons.Filled.Home else Icons.Outlined.Home,
-                        contentDescription = "Home",
-                        modifier = Modifier.size(24.dp) // Tamaño estándar para íconos
-                    )
-                },
-                selected = selectedItemIndex == "Home",
-                onClick = {
-                    selectedItemIndex = "Home"
-                    navController.navigate(Screen.HomeScreen)
-                }
-            )
-            NavigationBarItem(
-                label = { Text(text = "Carrito") },
-                icon = {
-                    Icon(
-                        imageVector = if (selectedItemIndex == "Carrito") Icons.Filled.ShoppingCart else Icons.Outlined.ShoppingCart,
-                        contentDescription = "Carrito",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedItemIndex == "Carrito",
-                onClick = {
-                    selectedItemIndex = "Carrito"
-                    navController.navigate(Screen.CarritoListScreen)
-                }
-            )
-            NavigationBarItem(
-                label = { Text(text = "Pedidos") },
-                icon = {
-                    Icon(
-                        imageVector = if (selectedItemIndex == "Pedidos") Icons.Filled.DateRange else Icons.Outlined.DateRange,
-                        contentDescription = "Pedidos",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedItemIndex == "Pedidos",
-                onClick = {
-                    selectedItemIndex = "Pedidos"
-                    navController.navigate(Screen.PedidoListScreen)
-                }
-            )
-            NavigationBarItem(
-                label = { Text(text = "Reviews") },
-                icon = {
-                    Icon(
-                        imageVector = if (selectedItemIndex == "Reviews") Icons.Filled.Star else Icons.Outlined.Star,
-                        contentDescription = "Reviews",
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedItemIndex == "Reviews",
-                onClick = {
-                    selectedItemIndex = "Review"
-                    navController.navigate(Screen.ReviewListScreen)
-                }
-            )
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = currentRoute == item.route,
+                    onClick = { navController.navigate(item.route) },
+                    label = {
+                        Text(text = item.title)
+                    },
+                    alwaysShowLabel = false,
+                    icon = {
+                        Icon(
+                            imageVector = if (currentRoute == item.route) item.selectedIcon else item.unselectedIcon,
+                            contentDescription = item.title
+                        )
+                    }
+                )
+            }
         }
     }
 }
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null,
+    val route: String
+)
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
