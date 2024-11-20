2b806408da16cfd5da755335264780833aa91d8a
Luis Manuel <luis_duarte@ucne.edu.do>
2024-11-20 13:54:17 -0600

    Actualiza

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/ProyectoFinalAplicada2NavHost.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/navigation/ProyectoFinalAplicada2NavHost.kt
@@ -193,6 +193,7 @@ fun ProyectoFinalAplicada2NavHost(
                     onDrawer = {
                         scope.launch {
                             drawerState.open()
+
                         }
                     }
                 )
