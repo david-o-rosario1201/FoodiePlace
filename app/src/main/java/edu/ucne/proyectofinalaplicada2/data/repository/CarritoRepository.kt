6dc2e9c38fc855c5874ba804b58e88e0647a70a4
HenzoWTF <Urenahenzo@gmail.com>
2024-11-23 19:15:26 -0400

    Agrega en CarritoRepository el metodo para guardar carrito en la base de datos local

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/repository/CarritoRepository.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/repository/CarritoRepository.kt
@@ -1,6 +1,8 @@
 package edu.ucne.proyectofinalaplicada2.data.repository
 
 import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDao
+import edu.ucne.proyectofinalaplicada2.data.local.dao.CarritoDetalleDao
+import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoDetalleEntity
 import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoEntity
 import edu.ucne.proyectofinalaplicada2.data.remote.Resource
 import edu.ucne.proyectofinalaplicada2.data.remote.dataSource.CarritoRemoteDataSource
@@ -12,16 +14,23 @@ import javax.inject.Inject
 
 class CarritoRepository @Inject constructor(
     private val carritoRemoteDataSource: CarritoRemoteDataSource,
-    private val carritoDao: CarritoDao
+    private val carritoDao: CarritoDao,
+    private val DetalleCarritoDao: CarritoDetalleDao
 ) {
     suspend fun getCarrito() = carritoRemoteDataSource.getCarrito()
-    suspend fun getCarritoById(id: Int) = carritoRemoteDataSource.getCarritoById(id)
+    suspend fun getCarritoById(id: Int) = carritoDao.getCarritoById(id)
     suspend fun addCarrito(carrito: CarritoDto) = carritoRemoteDataSource.postCarrito(carrito)
     suspend fun deleteCarrito(id: Int) = carritoRemoteDataSource.deleteCarrito(id)
 
     fun getCarritoss(): Flow<Resource<List<CarritoEntity>>> = flow {
         try{emit(Resource.Loading())
             val carrito = carritoRemoteDataSource.getCarrito()
+
+            carrito.forEach {
+                carritoDao.save(
+                    it.toCarritoEntity()
+                )
+            }
 
             carritoDao.getAll().collect{carritoLocal ->
                 emit(Resource.Success(carritoLocal))
@@ -37,4 +46,22 @@ class CarritoRepository @Inject constructor(
             }
         }
     }
+
+    suspend fun addCarritoDetalle(detalle: CarritoDetalleEntity) {
+        DetalleCarritoDao.save(detalle)
+    }
+
+    fun getCarritoDetalles(carritoId: Int): Flow<List<CarritoDetalleEntity>> {
+        return DetalleCarritoDao.getCarritoDetalles(carritoId)
+    }
+
+    suspend fun clearCarrito(carritoId: Int) {
+        DetalleCarritoDao.clearCarrito(carritoId)
+    }
 }
+private fun CarritoDto.toCarritoEntity() = CarritoEntity(
+    carritoId = carritoId,
+    usuarioId = usuarioId,
+    fechaCreacion = fechaCreacion,
+    pagado, carritoDetalle.toMutableList()
+)
