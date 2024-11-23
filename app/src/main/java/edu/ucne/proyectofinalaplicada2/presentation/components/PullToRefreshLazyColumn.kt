2f69bfd6d1189ae449939027adacc0d403a00329
david-o-rosario1201 <finnnn023@gmail.com>
2024-11-22 23:57:56 -0400

    Refactoriza componente PullToRefreshLazyColumn para que no sea el encargado de la iteración las columnas

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/components/PullToRefreshLazyColumn.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/components/PullToRefreshLazyColumn.kt
@@ -2,16 +2,9 @@
 
 package edu.ucne.proyectofinalaplicada2.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
@@ -20,46 +13,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import edu.ucne.proyectofinalaplicada2.ui.theme.ProyectoFinalAplicada2Theme
 
 @Composable
-fun <I, E> PullToRefreshLazyColumn(
-    items: List<I>,
-    content: @Composable (I) -> Unit,
+fun PullToRefreshLazyColumn(
     isRefreshing: Boolean,
-    onRefresh: (E) -> Unit,
-    event: E,
-    modifier: Modifier = Modifier,
-    lazyListState: LazyListState = rememberLazyListState()
+    onRefresh: () -> Unit,
+    content: @Composable () -> Unit
 ){
     val pullToRefreshState = rememberPullToRefreshState()
 
     Box(
-        modifier = modifier
+        modifier = Modifier
             .nestedScroll(pullToRefreshState.nestedScrollConnection)
-    ){
-        LazyColumn(
-            state = lazyListState,
-            contentPadding = PaddingValues(8.dp),
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ){
-            items(items){
-                content(it)
-            }
-        }
+            .fillMaxSize()
+    ) {
+        content()
 
-        if(pullToRefreshState.isRefreshing){
+        if (pullToRefreshState.isRefreshing) {
             LaunchedEffect(key1 = true) {
-                onRefresh(event)
+                onRefresh()
             }
         }
 
         LaunchedEffect(isRefreshing) {
-            if(isRefreshing){
+            if (isRefreshing) {
                 pullToRefreshState.startRefresh()
-            } else{
+            } else {
                 pullToRefreshState.endRefresh()
             }
         }
@@ -77,13 +57,9 @@ fun <I, E> PullToRefreshLazyColumn(
 fun PullToRefreshLazyColumnPreview(){
     ProyectoFinalAplicada2Theme {
         PullToRefreshLazyColumn(
-            items = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-            content = {
-                Text(text = it.toString())
-            },
             isRefreshing = false,
             onRefresh = {},
-            event = {}
+            content = {}
         )
     }
 }
