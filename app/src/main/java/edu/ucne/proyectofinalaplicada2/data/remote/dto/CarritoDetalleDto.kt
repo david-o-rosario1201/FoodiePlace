2d4fc532ca3295161fd588564eff59d5f0c215c5
HenzoWTF <Urenahenzo@gmail.com>
2024-11-24 13:44:55 -0400

    Cambia que todos los datos de CarritoDetalleDto sean nulos

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/CarritoDetalleDto.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/data/remote/dto/CarritoDetalleDto.kt
@@ -4,11 +4,11 @@ import java.math.BigDecimal
 
 data class CarritoDetalleDto (
     val carritoDetalleId : Int? = null,
-    val carritoId : Int,
-    val productoId : Int,
-    val cantidad : Int,
-    val precioUnitario : BigDecimal,
-    val impuesto : BigDecimal,
-    val subTotal : BigDecimal,
-    val propina : BigDecimal
+    val carritoId : Int? = null,
+    val productoId : Int? = null,
+    val cantidad : Int? = null,
+    val precioUnitario : BigDecimal? = null,
+    val impuesto : BigDecimal? = null,
+    val subTotal : BigDecimal? = null,
+    val propina : BigDecimal? = null,
 )
