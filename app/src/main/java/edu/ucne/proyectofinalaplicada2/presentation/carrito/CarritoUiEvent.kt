9f6f09675a99b73e4e677e9b02912b9b33016bc5
HenzoWTF <Urenahenzo@gmail.com>
2024-11-24 22:31:18 -0400

    Agrega mas object en eñ CarritoUiEvent para hacerlo MVI

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/carrito/CarritoUiEvent.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/carrito/CarritoUiEvent.kt
@@ -1,10 +1,17 @@
 package edu.ucne.proyectofinalaplicada2.presentation.carrito
 
-import edu.ucne.proyectofinalaplicada2.presentation.Reseñas.ReviewUiEvent
+import edu.ucne.proyectofinalaplicada2.data.local.entities.CarritoDetalleEntity
 
-interface CarritoUiEvent {
-    data class IsRefreshingChanged(val isRefreshing: Boolean): CarritoUiEvent
-    object Save: CarritoUiEvent
-    object Delete: CarritoUiEvent
-    data object Refresh: CarritoUiEvent
+sealed class CarritoUiEvent {
+    object LoadCarritoDetalles : CarritoUiEvent()
+    object LoadCarritos : CarritoUiEvent()
+    object SaveCarrito : CarritoUiEvent()
+    object DeleteCarrito : CarritoUiEvent()
+    object LimpiarCarrito : CarritoUiEvent()
+    data class RealizarPago(val tarjetaId: Int, val pedidoId: Int) : CarritoUiEvent()
+    data class AgregarProducto(val producto: CarritoDetalleEntity, val cantidad: Int) : CarritoUiEvent()
+    data class EliminarProducto(val productoId: Int) : CarritoUiEvent()
+    object Refresh : CarritoUiEvent()
+    data class IsRefreshingChanged(val isRefreshing: Boolean): CarritoUiEvent()
+
 }
