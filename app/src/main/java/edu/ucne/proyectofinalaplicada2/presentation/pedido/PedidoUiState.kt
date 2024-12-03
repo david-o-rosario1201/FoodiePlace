bdf690aa1b9f8b9f67ef193dd9315e1fe620ea11
david-o-rosario1201 <finnnn023@gmail.com>
2024-12-02 20:19:02 -0400

    Agrega la variable usuarioRol a PedidoUiState

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pedido/PedidoUiState.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/pedido/PedidoUiState.kt
@@ -6,6 +6,7 @@ import java.math.BigDecimal
 import java.util.Date
 
 data class PedidoUiState(
+    val usuarioRol: String? = "",
     val pedidoId: Int? = null,
     val usuarioId: Int? = 0,
     val fechaPedido: Date? = Date(),
