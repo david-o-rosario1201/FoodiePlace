af0031b77159022a44792982aa485425447568ad
Luis Manuel <luis_duarte@ucne.edu.do>
2024-12-04 07:59:14 -0600

    Agrega manejo de Roles y elimina Nullable

--- app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/reservaciones/ReservacionesUiState.kt
+++ app/src/main/java/edu/ucne/proyectofinalaplicada2/presentation/reservaciones/ReservacionesUiState.kt
@@ -6,14 +6,16 @@ import java.util.Date
 data class ReservacionesUiState(
     val reservacionId: Int? = null,
     val usuarioId: Int? = null,
+    val usuarioRol: String? = "",
     val fechaReservacion: Date = Date(),
     val numeroPersonas: Int = 0,
     val estado: String = "Pendiente",
     val reservaciones: List<ReservacionesEntity> = emptyList(),
     val numeroMesa: Int = 1,
-    val horaReservacion: Date? = Date(),
+    val horaReservacion: Date = Date(),
     val isLoading: Boolean = false,
     val success: Boolean = false,
     val errorMensaje: String? = null,
-    val isRefreshing: Boolean = false
-)
+    val isRefreshing: Boolean = false,
+
+    )
